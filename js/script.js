077e0f45e419a455a93ad42f683b9dd6c60d4cfa
Vitor Lobo <vtloboss@hotmail.com>
2022-09-14 08:47:08 -0300

    Adicionada parte de JS para o formulario

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 import ScrollAnimate from "./modules/scroll-animate.js";
 import MobileMenu from "./modules/mobile-menu.js";
+import FormMessage from "./modules/form-message.js";
 
 const scrollAnimate = new ScrollAnimate('[data-animate= "scroll"]');
 scrollAnimate.init();
 
-const mobileMenu = new MobileMenu('[data-menu="hamburguer"]', '[data-menu="lista"]');
+const mobileMenu = new MobileMenu(
+  '[data-menu="hamburguer"]',
+  '[data-menu="lista"]'
+);
 mobileMenu.init();
+
+const formMessage = new FormMessage({
+  form: ".formjs",
+  button: "#enviar",
+  error:
+    "<div id='form-error'><h2>Oops! Um erro ocorreu.</h2><p>Tente de novo ou envie um e-mail para contato@blueheaven.com</p></div>",
+  success:
+    "<div id='form-success'><h2>Mensagem enviada.</h2><p>Agradecemos o contato e retornaremos em breve</p></div>",
+});
+formMessage.init();
